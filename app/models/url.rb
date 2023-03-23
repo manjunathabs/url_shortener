db4c4baa79bcf2555a9fca6763efa16169969f04
manju <manjunathabs@parashmis.com>
2023-03-23 16:22:45 +0530

    SecureRandom gem to gererate random url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,11 @@
 class Url < ApplicationRecord
-  
+  validates :original, presence: true, format: URI::regexp(%w[http https])
+  validates :short, presence: true, uniqueness: true
+
+  def self.generate_short_url
+    loop do
+      short = SecureRandom.hex(3)
+      return short unless Url.exists?(short: short)
+    end
+  end
 end
-
-
