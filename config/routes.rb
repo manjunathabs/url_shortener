7864a8cccd01a9e84f61ffde189eccd220dcd11f
manju <manjunathabs@parashmis.com>
2023-03-23 22:37:29 +0530

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'urls/new'
+ #  get 'urls/new'
+
+  root 'urls#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'urls/create'
 
